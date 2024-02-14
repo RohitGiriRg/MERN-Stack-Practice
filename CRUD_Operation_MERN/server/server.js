9365ca9285f76f462f2aef1f605163d8a33ffdf3
rohit giri <rohitgirirg7027@gmail.com>
2024-02-14 22:15:55 +0530

    created an messaging funtionality

--- CRUD_Operation_MERN/server/server.js
+++ CRUD_Operation_MERN/server/server.js
@@ -9,30 +9,28 @@ const port = 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/collectionTodo");
+mongoose.connect("mongodb://localhost:27017/particulardatas");
 
-const todoData = new mongoose.Schema({
+const TodoCollection = new mongoose.Schema({
   title: String,
   description: String,
 });
 
-const Todo = mongoose.model("particularData", todoData);
+const Todo = mongoose.model("particulardatas", TodoCollection);
 
-// Creating Todo
 app.post("/api/todos", async (req, res) => {
   try {
     const { title, description } = req.body;
 
     const newTodo = new Todo({ title, description });
     await newTodo.save();
-
     res.json(newTodo);
   } catch (error) {
-    // console.log(error);
+    console.log(error);
     res.status(500).json({ error: error.message });
   }
 });
 
 app.listen(port, () => {
-  console.log(`your server is running on port ${port}`);
+  console.log(`You are listening to the port ${port}`);
 });
