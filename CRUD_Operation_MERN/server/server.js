05f06249f7ece5cd395dc84b1c89cfd8385c1d29
rohit giri <rohitgirirg7027@gmail.com>
2024-02-15 11:40:51 +0530

    Created CRUD operation Successfully

--- CRUD_Operation_MERN/server/server.js
+++ CRUD_Operation_MERN/server/server.js
@@ -6,10 +6,11 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
+app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://localhost:27017/particulardatas");
+mongoose.connect("mongodb://127.0.0.1:27017/particulardatas");
 
 const TodoCollection = new mongoose.Schema({
   title: String,
@@ -18,6 +19,7 @@ const TodoCollection = new mongoose.Schema({
 
 const Todo = mongoose.model("particulardatas", TodoCollection);
 
+// Creating Todos
 app.post("/api/todos", async (req, res) => {
   try {
     const { title, description } = req.body;
@@ -27,6 +29,53 @@ app.post("/api/todos", async (req, res) => {
     res.json(newTodo);
   } catch (error) {
     console.log(error);
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Getting the todo present in the database
+app.get("/api/todos", async (req, res) => {
+  try {
+    const previousData = await Todo.find();
+    if (!previousData) {
+      res.json("No Data found");
+    } else {
+      res.json(previousData);
+    }
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Updating the specific todos by id.
+
+app.put("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const { title, description } = req.body;
+
+    console.log("asdf");
+    const updatedTodo = await Todo.findByIdAndUpdate(id, {
+      title,
+      description,
+    });
+
+    if (!updatedTodo) {
+      return res.status(404).json({ error: "Todo not found" });
+    }
+    res.json(updatedTodo);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
+
+// Deleting the todos
+app.delete("/api/todos/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const deletedId = await Todo.findByIdAndDelete(id);
+    res.json({ message: `Deleted todo with id : ${deletedId}` });
+  } catch (error) {
     res.status(500).json({ error: error.message });
   }
 });
