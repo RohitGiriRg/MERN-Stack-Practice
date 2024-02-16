f058a8afa7749c56d780887ca930e7c5c076e48a
rohit giri <rohitgirirg7027@gmail.com>
2024-02-16 23:34:50 +0530

    create client side for CRUD app

--- CRUD_Operation_MERN/server/server.js
+++ CRUD_Operation_MERN/server/server.js
@@ -6,11 +6,10 @@ const cors = require("cors");
 const app = express();
 const port = 5000;
 
-app.use(express.json());
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/particulardatas");
+mongoose.connect("mongodb://localhost:27017/particulardatas");
 
 const TodoCollection = new mongoose.Schema({
   title: String,
@@ -33,6 +32,10 @@ app.post("/api/todos", async (req, res) => {
   }
 });
 
+app.listen(port, () => {
+  console.log(`You are listening to the port ${port}`);
+});
+
 // Getting the todo present in the database
 app.get("/api/todos", async (req, res) => {
   try {
@@ -45,41 +48,4 @@ app.get("/api/todos", async (req, res) => {
   } catch (error) {
     res.status(500).json({ error: error.message });
   }
-});
-
-// Updating the specific todos by id.
-
-app.put("/api/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title, description } = req.body;
-
-    console.log("asdf");
-    const updatedTodo = await Todo.findByIdAndUpdate(id, {
-      title,
-      description,
-    });
-
-    if (!updatedTodo) {
-      return res.status(404).json({ error: "Todo not found" });
-    }
-    res.json(updatedTodo);
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-// Deleting the todos
-app.delete("/api/todos/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const deletedId = await Todo.findByIdAndDelete(id);
-    res.json({ message: `Deleted todo with id : ${deletedId}` });
-  } catch (error) {
-    res.status(500).json({ error: error.message });
-  }
-});
-
-app.listen(port, () => {
-  console.log(`You are listening to the port ${port}`);
 });
